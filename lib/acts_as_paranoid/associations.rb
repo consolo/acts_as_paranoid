53cdc4cc82810307e81eaa21157d8e8a369efe9c
Andrew Coleman <penguincoder@gmail.com>
2016-02-24 13:11:14 -0500

    use symbols

--- lib/acts_as_paranoid/associations.rb
+++ lib/acts_as_paranoid/associations.rb
@@ -10,7 +10,7 @@ module ActsAsParanoid
     module ClassMethods
       def belongs_to_with_deleted(target, scope = nil, options = {})
         with_deleted = (scope.is_a?(Hash) ? scope : options).delete(:with_deleted)
-        result = belongs_to_without_deleted(target, scope, options)
+        result = belongs_to_without_deleted(target.to_sym, scope, options)
 
         if with_deleted
           if result.is_a? Hash
